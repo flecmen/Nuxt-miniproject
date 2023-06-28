604c26686483b4b3fcba90e96be8b6c8d2541fa8
David Flek <davidovkyflekovky@gmail.com>
2023-06-28 18:32:23 +0200

    basic product styles

--- components/products/Product.vue
+++ components/products/Product.vue
@@ -2,44 +2,64 @@
 import { Product } from '@prisma/client';
 
 defineProps<{
-    product: Product;
+  product: Product;
 }>()
 
 </script>
 
 <template>
-    <div class="product">
-        <div class="image_container">
-            <div v-if="product.image">
-                <img :src="product.image" alt="">
-            </div>
-            <div v-else>
-                <!--  -->
-            </div>
-        </div>
-    
-        <div class="product_info">
-            <h1>{{product.title}}</h1>
-            <p>{{ product.price }}</p>
-            <p>{{ product.description }}</p>
-        </div>
+  <div class="product">
+    <div class="image-container">
+      <img v-if="product.image" :src="product.image" alt="Product Image" class="product-image" />
+      <div v-else class="placeholder-image"></div>
     </div>
+
+    <div class="product-info">
+      <h1>{{ product.title }}</h1>
+      <p class="price">{{ product.price }}</p>
+      <p class="description">{{ product.description }}</p>
+    </div>
+  </div>
 </template>
 
-
-
 <style scoped>
-.product_info{
-display: flex;
-  align-items: flex-start;
+.product {
+  display: flex;
+  padding-bottom: 10px;
 }
 
-.image_container{
+.image-container {
   margin-right: 20px;
+  object-fit: cover;
 }
 
-.product{
-    display: flex;
-    padding-bottom: 10px;
+.product-image {
+  height: 200px; /* Adjust the height as desired */
+  object-fit: cover; /* Maintain aspect ratio and cover the container */
+}
+
+.placeholder-image {
+  width: 100%;
+  height: 200px;
+  background-color: #f3f3f3;
+}
+
+.product-info {
+  display: flex;
+  flex-direction: column;
+}
+
+h1 {
+  font-size: 24px;
+  margin-top: 0;
+}
+
+.price {
+  font-weight: bold;
+  margin: 10px 0;
+}
+
+.description {
+  margin-bottom: 0;
 }
 </style>
--- pages/products.vue
+++ pages/products.vue
@@ -1,21 +1,26 @@
 <!-- Add a new page to the application (/products) that would display images, their titles, descriptions, 
     prices and images. Focus primarily on the layout, no need to worry about colors/backgrounds/etc. (You choose the design) -->
-<template>
-    <div>
-        <Product 
-            v-for="product in data"
-            :key="product.id"
-            :product="product"
-        />
+    <script setup>
+    const { data } = await useFetch('/api/products');
+  </script>
+  
+  <template>
+    <div class="product-grid">
+      <Product v-for="product in data" :key="product.id" :product="product" />
     </div>
-    {{ data }}
-</template>
-
-<script setup>
-const {data} = await useFetch('/api/products')
-
-</script>
-
-<style scoped>
-
-</style>
+  </template>
+  
+  <style scoped>
+  .product-grid {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+    gap: 20px;
+  }
+  
+  @media (max-width: 768px) {
+    .product-grid {
+      grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+    }
+  }
+  </style>
+  
